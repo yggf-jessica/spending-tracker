c9ea333d7a5d0026b6db31dafe0df4b18af17e01
Ori-Nrk <u6530224@au.edu>
2025-07-23 15:44:40 +0700

    index fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
